4d04480c8a8199a9649c3cd7d9c4c4475fb222a9
rehakui <khrnyk@gmail.com>
2026-03-25 11:28:38 +0900

    day5 urud

--- app.py
+++ app.py
@@ -2,6 +2,45 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+users = []
+next_id = 1
+
+# 取得
+@app.route("/users", methods=["GET"])
+def get_users():
+    return users
+
+# 追加
+@app.route("/users", methods=["POST"])
+def add_user():
+    global next_id
+    data = request.json
+    user = {
+        "id": next_id,
+        "name": data["name"]
+    }
+    users.append(user)
+    next_id += 1
+
+    return user, 201
+
+# 削除
+@app.route("/users/<int:index>", methods=["DELETE"])
+def delete_user(index):
+    if index < len(users):
+        deleted = users.pop(index)
+        return deleted
+    return {"error": "not found"}, 404
+
+# 更新
+@app.route("/users/<int:index>", methods=["PUT"])
+def update_user(index):
+    if index < len(users):
+        data = request.json
+        users[index]["name"] = data["name"]
+        return users[index]
+    return {"error": "not found"}, 404
+
 @app.route("/")
 def home():
     return "Home"
@@ -23,5 +62,13 @@ def login():
 def search():
     keyword = request.args.get("q")
     return f"search: {keyword}"
+
+@app.route("/status")
+def status():
+    return {"status": "ok"}, 200
+
+@app.route("/error")
+def notfound():
+    return {"error": "something wrong"}, 500
 
 app.run(debug=True)
